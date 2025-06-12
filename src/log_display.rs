2aa7480a65d338e7dae2db30dea2d1310d3effed
mdc <galacticwingcommander@gmail.com>
2025-06-12 09:28:41 +0800

    Rendering of trips working nicely. Still to add all event sub-data.

--- src/log_display.rs
+++ src/log_display.rs
@@ -1,12 +1,8 @@
 // Log display on UI.
 
-use log::info;
-use log::warn;
-
 use crate::egui::Color32;
- use crate::egui::RichText;
+use crate::egui::RichText;
 use crate::egui::{ScrollArea, Ui};
-use regex::Regex;
 
 use crate::scraper::ScrapedData;
 
@@ -40,74 +36,112 @@ impl UiState {
 // The display entry point.
 pub fn render_scraped_data(ui: &mut Ui, ui_state: &mut UiState, scraped_data: &[ScrapedData], available_height: f32) {
     ScrollArea::vertical()
-        .max_height(available_height - 10.0)
-        .show(ui, |ui| {
+    .max_height(available_height - 10.0)
+    .show(ui, |ui| {
 
-            // Set content area where scroll bar will appear.
-            ui.set_min_width(500.0);
-            
-            if !ui_state.display_ready || scraped_data.is_empty() {
-                return;
-            }
+        // Set content area where scroll bar will appear.
+        ui.set_min_width(500.0);
 
-            // Process scraped data in order.
-            for item in scraped_data {
-                if item.event_type == "SIGNON" {
-                    info!("Processing SIGNON event.");
+        // If UI not ready or nothing to render then return.
+        if !ui_state.display_ready || scraped_data.is_empty() {
+            return;
+        }
 
-                    let ev_info = ungroup_event_data("SIGNON".to_string(), &item.ev_detail);
+        let mut current_trip_header: Option<&ScrapedData> = None;
+        let mut trip_events: Vec<(usize, &ScrapedData)> = Vec::new();
+        let mut in_trip = false;
 
-                    ui.collapsing(RichText::new(format!("Trip {:} - ({:})", item.trip_num, &item.date_time)).color(Color32::WHITE), |ui| {
-                        ui.collapsing(RichText::new("SIGNON").color(Color32::GREEN), |ui| {
-                        for (key, value) in &ev_info {
-                            ui.horizontal(|ui| {
-                            ui.label(format!("{}:", key));
-                            ui.colored_label(Color32::YELLOW, value);
-                            });
+        for (index, item) in scraped_data.iter().enumerate() {
+            match item.event_type.as_str() {
+                "SIGNON" => {
+                    // Start a new trip.
+                    current_trip_header = Some(item);
+                    trip_events.clear();
+                    trip_events.push((index, item));
+                    in_trip = true;
+                }
+                "TRIP" => {
+                    if in_trip {
+                        // Add TRIP event to current trip and then render the complete trip.
+                        trip_events.push((index, item));
+                        if let Some(trip_data) = current_trip_header {
+                            render_trip_section(ui, trip_data, &trip_events);
                         }
-                    });
-                });
+                        // End the trip.
+                        current_trip_header = None;
+                        trip_events.clear();
+                        in_trip = false;
+                    } else {
+                        // TRIP event without SIGNON,
+                        // Out of trip events, generally at the start of a log file.
+                        // These events get displayed at the top level.
+                        render_top_level_event(ui, index, item);
+                    }
+                }
+                _ => {
+                    if in_trip {
+                        // Add event to current trip.
+                        trip_events.push((index, item));
+                    } else {
+                        // Not in trip.
+                        // Display at top level (in between TRIP and SIGNON).
+                        render_top_level_event(ui, index, item);
+                    }
+                }
+            }
+        }
+
+        // Handle case where data ends without a TRIP event (incomplete trip).
+        if in_trip && !trip_events.is_empty() {
+            if let Some(trip_data) = current_trip_header {
+                render_trip_section(ui, trip_data, &trip_events);
             }
         }
     });
 }
 
-// Function to expand on the scraped data, i.e. the grouped log data
-// not expanded on initially.
-fn ungroup_event_data(event_type: String, sub_data: &str) -> Vec<(String, String)> {
-    // Initialise result vector.
-    let mut result = Vec::new();
-
-    // Search for the event sub-data for the SIGNON event.
-    if event_type == "SIGNON" {
-        // Get the event sub-data.
-        let sub_signon_pattern = Regex::new(r"([-\*\+0-9]+) ([0-9a-fA-F]+) (.+?) ([0-9]+) ([0-9]+) ([0-9]+) (.+?)$");
-        let mut _found_sub = false;
-
-        if let Some(captures) = sub_signon_pattern.expect("REASON").captures(sub_data) {
-
-            if let Some(driver_id) = captures.get(1) {
-                result.push(("Operator id".to_string(), driver_id.as_str().to_string()));
-            }
-            if let Some(card_id) = captures.get(2) {
-                result.push(("Card id".to_string(), card_id.as_str().to_string()));
-            }
-            if let Some(sign_stat) = captures.get(3) {
-                result.push(("Result".to_string(), sign_stat.as_str().to_string()));
-            }
-            if let Some(bits_read) = captures.get(4) {
-                result.push(("Bits read".to_string(), bits_read.as_str().to_string()));
-            }
-            if let Some(keyboard) = captures.get(5) {
-                result.push(("Keyboard".to_string(), keyboard.as_str().to_string()));
-            }
-            if let Some(card_reader) = captures.get(6) {
-                result.push(("Card reader".to_string(), card_reader.as_str().to_string()));
-            }
-            else {
-                warn!("Failed to extract sub-data from SIGNON");
+// Helper function to render a complete trip.
+fn render_trip_section(ui: &mut Ui, trip_data: &ScrapedData, trip_events: &[(usize, &ScrapedData)]) {
+    ui.collapsing(
+        RichText::new(format!("Trip {} at {}", trip_data.trip_num, &trip_data.date_time))
+            .color(Color32::WHITE), 
+        |ui| {
+            // Display all events for this trip.
+            for (index, item) in trip_events {
+                let event_id = format!("{}_{}", index, &item.event_type);
+                ui.push_id(&event_id, |ui| {
+                    ui.collapsing(
+                        RichText::new(&item.event_type).color(Color32::GREEN), 
+                        |ui| {
+                            for (key, value) in &item.ev_detail {
+                                ui.horizontal(|ui| {
+                                    ui.label(format!("{}:", key));
+                                    ui.colored_label(Color32::YELLOW, value);
+                                });
+                            }
+                        }
+                    );
+                });
             }
         }
-    }   
-    result
+    );
+}
+
+// Helper function to render top-level events,
+// that is, events outside of a trip.
+fn render_top_level_event(ui: &mut Ui, index: usize, item: &ScrapedData) {
+    let event_id = format!("{}_{}", index, &item.event_type);
+    ui.push_id(&event_id, |ui| {
+        ui.collapsing(
+            RichText::new(&item.event_type).color(Color32::GREEN),
+            |ui| {
+                for (key, value) in &item.ev_detail {
+                    ui.horizontal(|ui| {
+                        ui.label(format!("{}:", key));
+                        ui.colored_label(Color32::YELLOW, value);
+                    });
+                }
+            }
+        );
+    });
 }
--- src/scraper.rs
+++ src/scraper.rs
@@ -1,6 +1,7 @@
 // Scraper structure and methods.
 
 use log::info;
+use log::warn;
 
 use regex::Regex;
 use std::fs::File;
@@ -23,10 +24,11 @@ pub enum FileDialogMessage {
 #[derive(Debug)]
 pub struct ScrapedData {
     pub date_time: String,
+    pub _on_trip: bool,
     pub _new_trip: bool,
     pub trip_num: String,
     pub event_type: String,
-    pub ev_detail: String,
+    pub ev_detail: Vec<(String, String)>,
 }
 
 // Scraper struct and methods.
@@ -154,7 +156,7 @@ impl Scraper {
     
         // Get the serial number of the controller.
         let sn_pattern = Regex::new(r"([0-9]{1,2}/[0-9]{2}/[0-9]{4}) ([0-9]{1,2}:[0-9]{2}:[0-9]{2}(?:\.\d{3})?(?: [AP]M)?)[:, ]UNIT ([0-9]+)$")?;
-        let mut _found_sn = false;
+        let mut found_sn = false;
         
         // Process file line by line,
         for line_result in reader.lines() {
@@ -162,17 +164,17 @@ impl Scraper {
             
             // Check if we should stop processing.
             if let Some(captures) = sn_pattern.captures(&line) {
-                _found_sn = true;
+                found_sn = true;
                 // Group 3 contains the serialnumber.
                 let sn_str = captures.get(3).unwrap().as_str();
                 self.controller_id = sn_str.to_string();
                 info!("Found controller s/n: {:0>6}", sn_str); 
             }
-            if _found_sn == true {
+            if found_sn == true {
                 break
             }
         }
-        if _found_sn == false {
+        if found_sn == false {
             info!("Failed to find controller serial number."); 
         }
 
@@ -214,7 +216,9 @@ impl Scraper {
 
         // Get the controller events.
         let ev_pattern = Regex::new(r"([0-9]{1,2}/[0-9]{2}/[0-9]{4}) ([0-9]{1,2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}) EVENT ([0-9]+) ([0-9]+) ([-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+) ([A-Z_]+) (.+)$")?;
-        let mut _event_count = 0;
+
+        // Track if we are in or out of trip.
+        let mut intrip = false;
 
         // Process file line by line.
         for line_result in reader.lines() {
@@ -222,22 +226,31 @@ impl Scraper {
             
             // Check for event pattern
             if let Some(captures) = ev_pattern.captures(&line) {
-                _event_count += 1;
                 
-                // Extract some key fields for logging.
+                // Extract key fields for logging.
                 let date = captures.get(1).unwrap().as_str();
                 let time = captures.get(2).unwrap().as_str();
                 let event_type = captures.get(10).unwrap().as_str();
                 let event_detail = captures.get(11).unwrap().as_str();
+                let ev_key_vals = ungroup_event_data(event_type.to_string(), event_detail);
                 let trip_id = captures.get(3).unwrap().as_str();
-                
+
+                // Keep track of on-trip state.
+                // SIGNON sets TRIP clears.
+                if event_type == "SIGNON" {
+                    intrip = true;
+                } else if event_type == "TRIP" {
+                    intrip = false;
+                }
+
                 // Create and populate the struct correctly
                 let ev_data = ScrapedData {
                     date_time: format!("{} {}", date, time),
-                    _new_trip: event_type == "SIGNON",
+                    _on_trip: intrip,
+                    _new_trip: intrip,
                     trip_num: trip_id.to_string(),
                     event_type: event_type.to_string(),
-                    ev_detail: event_detail.to_string(),
+                    ev_detail: ev_key_vals,
                 };
 
                 // Push the struct onto the vector.
@@ -265,6 +278,101 @@ impl Scraper {
     pub fn get_processing_status(&self) -> &str {
         &self.processing_status
     }
+}
+
+// Function to expand on the scraped data.
+fn ungroup_event_data(event_type: String, sub_data: &str) -> Vec<(String, String)> {
+    // Initialise result vector.
+    let mut result = Vec::new();
+
+    // Search for the event sub-data for the SIGNON event.
+    match event_type.as_str() {
+        "SIGNON" => {
+            let sub_signon_pattern = Regex::new(r"([-\*\+0-9]+) ([0-9a-fA-F]+) (.+?) ([0-9]+) ([0-9]+) ([0-9]+) (.+?)$")
+                .expect("Invalid SIGNON regex pattern");
+
+            if let Some(captures) = sub_signon_pattern.captures(sub_data) {
+                if let Some(driver_id) = captures.get(1) {
+                    result.push(("Operator id".to_string(), driver_id.as_str().to_string()));
+                }
+                if let Some(card_id) = captures.get(2) {
+                    result.push(("Card id".to_string(), card_id.as_str().to_string()));
+                }
+                if let Some(sign_stat) = captures.get(3) {
+                    result.push(("Result".to_string(), sign_stat.as_str().to_string()));
+                }
+                if let Some(bits_read) = captures.get(4) {
+                    result.push(("Bits read".to_string(), bits_read.as_str().to_string()));
+                }
+                if let Some(keyboard) = captures.get(5) {
+                    result.push(("Keyboard".to_string(), keyboard.as_str().to_string()));
+                }
+                if let Some(card_reader) = captures.get(6) {
+                    result.push(("Card reader".to_string(), card_reader.as_str().to_string()));
+                }
+            } else {
+                warn!("Failed to extract sub-data from SIGNON");
+            }
+        },
+
+        // Search for the event sub-data for the ZONECHANGE event.
+        "ZONECHANGE" => {
+            let sub_zone_pattern = Regex::new(r"([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)(.*)$")
+                .expect("Invalid ZONECHANGE regex pattern");
+
+            if let Some(captures) = sub_zone_pattern.captures(sub_data) {
+                if let Some(trip_id) = captures.get(1) {
+                    result.push(("Trip id".to_string(), trip_id.as_str().to_string()));
+                }
+                if let Some(from_zone) = captures.get(2) {
+                    result.push(("From zone".to_string(), from_zone.as_str().to_string()));
+                }
+                if let Some(to_zone) = captures.get(3) {
+                    result.push(("To zone".to_string(), to_zone.as_str().to_string())); // FIXED: Label
+                }
+                if let Some(zone_output) = captures.get(4) {
+                    result.push(("Zone output".to_string(), zone_output.as_str().to_string()));
+                }
+            } else {
+                warn!("Failed to extract sub-data from ZONECHANGE");
+            }
+        },
+
+        // Search for the event sub-data for the TRIP event.
+        "TRIP" => {
+            let sub_trip_pattern = Regex::new(r"([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)(.*)$")
+                .expect("Invalid TRIP regex pattern");
+
+            if let Some(captures) = sub_trip_pattern.captures(sub_data) {
+                if let Some(trip_id) = captures.get(1) {
+                    result.push(("Trip id".to_string(), trip_id.as_str().to_string()));
+                }
+                if let Some(time_fwd) = captures.get(2) {
+                    result.push(("Time fwd".to_string(), time_fwd.as_str().to_string()));
+                }
+                if let Some(time_rev) = captures.get(3) {
+                    result.push(("Time rev".to_string(), time_rev.as_str().to_string()));
+                }
+                if let Some(time_idle) = captures.get(4) {
+                    result.push(("Time idle".to_string(), time_idle.as_str().to_string()));
+                }
+                if let Some(max_idle) = captures.get(5) {
+                    result.push(("Max idle".to_string(), max_idle.as_str().to_string()));
+                }
+                if let Some(time_on_seat) = captures.get(6) {
+                    result.push(("Time on seat".to_string(), time_on_seat.as_str().to_string()));
+                }
+            } else {
+                warn!("Failed to extract sub-data from TRIP");
+            }
+        },
+        _ => {
+            // Handle other event types or add a default case
+            result.push(("Raw data".to_string(), sub_data.to_string()));
+        }
+    }
+
+    result
 }
 
 // Implement Default for way to create a 'blank' instance.
