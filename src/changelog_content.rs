f57d2814ebe5c3a8e94f95fcca61b4823e45ea87
mdc <galacticwingcommander@gmail.com>
2025-08-02 18:59:52 +0800

    Changelog updated.

--- src/changelog_content.rs
+++ src/changelog_content.rs
@@ -15,6 +15,6 @@ pub fn draw_changelog_content(ui: &mut egui::Ui, _app: &mut MyApp) {
         ui.label("Includes all event sub-data, as well as gps data.");
         ui.label("Options to show diagnostic events, and out of trip events.");
         ui.label("Support for light and dark display modes.");
-        ui.label("Plotting of gps breadcrumb trails included, with and without OSM tiles.");
+        ui.label("Plotting of gps breadcrumb trails included, including with street view or satellite view tiles.");
     });
 }
